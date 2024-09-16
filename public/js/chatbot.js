83a12c983208ebd160170d2d7f3d3f329cf8b2d4
Shakshi Vishwakarma <vishwakarmasakshi80@gmail.com>
2024-09-16 19:31:43 +0530

    changes in index.js chatbot

--- public/js/chatbot.js
+++ public/js/chatbot.js
@@ -5,8 +5,10 @@ const chatbotWindow = document.querySelector(".chatbot-window")
 
 messageIcon.addEventListener("click",()=>{
    chatbotWindow.style.display = "flex";
+   messageIcon.style.display = 'none'
 });
 
 closeButton.addEventListener("click",()=>{
    chatbotWindow.style.display = "none";
+   messageIcon.style.display = 'flex'
 });
